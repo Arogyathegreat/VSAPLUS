86256e35a65266d1fe59c8d0ef554178719bd6ca
Arogya <kharelarogya@hotmail.com>
2019-10-12 17:39:32 +0900

    passwordreset 수정

--- app/src/main/java/com/example/vsaplus/PasswordResetFragment.java
+++ app/src/main/java/com/example/vsaplus/PasswordResetFragment.java
@@ -1,16 +1,22 @@
 package com.example.vsaplus;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,43 +29,79 @@ import com.google.firebase.auth.FirebaseAuth;
  */
 public class PasswordResetFragment extends Fragment {
     FirebaseAuth mAuth;
-    EditText editText = (EditText)getActivity().findViewById(R.id.email);
+    EditText resetEmail;
 
     public static PasswordResetFragment newInstance() {
         return new PasswordResetFragment();
     }
     public void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
         mAuth = FirebaseAuth.getInstance();
+
     }
 
-    public void buttonclicked(View v){
-        String emailAddress;
-        if ( editText.getText().toString().length() == 0 ) {
-            Toast.makeText(getActivity(),"put your email above the button",Toast.LENGTH_LONG);
-            return;
-        } else {
-            emailAddress = editText.getText().toString();
-        }
-
-        mAuth.sendPasswordResetEmail(emailAddress).addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(getActivity(),"password reset email has sent. check your email!",Toast.LENGTH_LONG);
-                }
-                else{
-                    Toast.makeText(getActivity(),task.getException().getMessage(),Toast.LENGTH_LONG);
-                }
-            }
-        });
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_password_reset, container, false);
+        View view = inflater.inflate(R.layout.fragment_password_reset, container, false);
+        resetEmail = (EditText)view.findViewById(R.id.email);
+
+        resetEmail.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if(i == EditorInfo.IME_ACTION_DONE){
+                    String email = resetEmail.getText().toString();
+                    buttonclicked(email);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+        return view;
     }
+
+    public void buttonclicked(String emailAddress){
+        if ( emailAddress.length() == 0 ) {
+            Toast.makeText(getActivity(),"Please type your email above",Toast.LENGTH_LONG).show();
+            return;
+
+        } else {
+
+            mAuth.sendPasswordResetEmail(emailAddress).addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getActivity(), "An email has been sent to your account!", Toast.LENGTH_LONG).show();
+                        thread.start();
+                    } else {
+                        Toast.makeText(getActivity(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+
+        resetEmail.clearFocus();
+        InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(resetEmail.getWindowToken(), 0);
+
+    }
+
+    Thread thread = new Thread(){
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(Toast.LENGTH_SHORT);
+                ((MainActivity) getActivity()).loadFragment(SignupFragment.newInstance());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    };
+
 
 }
