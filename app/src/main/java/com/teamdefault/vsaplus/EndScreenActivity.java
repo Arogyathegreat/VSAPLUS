4afc769dcda427d2ae023e7767895ad898f4a56f
okcdbu <okcdbu@gmail.com>
2019-11-15 12:09:07 +0900

    score system re-upload!

--- app/src/main/java/com/teamdefault/vsaplus/EndScreenActivity.java
+++ app/src/main/java/com/teamdefault/vsaplus/EndScreenActivity.java
@@ -1,5 +1,6 @@
 package com.teamdefault.vsaplus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,7 +9,15 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.vsagames.demo.GameTestActivity;
 
 public class EndScreenActivity extends AppCompatActivity {
@@ -41,6 +50,26 @@ public class EndScreenActivity extends AppCompatActivity {
             scoreView.setText(test);
         }
         Log.d(TAG, "score is: "+ scoreUnity);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users");
+        if(user!=null){
+            myRef.child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    Long scoredata = (Long)dataSnapshot.child("score").getValue();
+                    if(scoredata == null){
+                        scoredata = 0L;
+                    }
+                    scoredata += Long.parseLong(scoreUnity+"");
+                    myRef.child(user.getUid()).child("score").setValue(scoredata);
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(getApplicationContext(),"Some error occured in score data pushing",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
     @Override
