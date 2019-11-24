36772ba3e828a27d657705219458c0b174a351e8
Arogya <kharelarogya@hotmail.com>
2019-11-24 17:09:57 +0900

    drag and drop to be continued......

--- app/src/main/java/com/teamdefault/vsaplus/DemoDragAndDropActivity.java
+++ app/src/main/java/com/teamdefault/vsaplus/DemoDragAndDropActivity.java
@@ -7,6 +7,7 @@ import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.DragEvent;
@@ -21,12 +22,16 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
 public class DemoDragAndDropActivity extends Activity implements View.OnDragListener, View.OnLongClickListener {
     private LinearLayout topcontainer;
+    private LinearLayout answercontainer;
+    private LinearLayout checkMarkContainer;
     private LottieAnimationView checkMark;
     private FirebaseFirestore rootref = FirebaseFirestore.getInstance();
     private String imageUrl1;
@@ -37,22 +42,44 @@ public class DemoDragAndDropActivity extends Activity implements View.OnDragList
 
 
     String[] answerText;
-    int superIndex;
+
     int score = 30;
+    int hashmapIndex;
+    Integer[] index;
+    HashMap <String[], String> answerMapping = new HashMap<>();
 
     int refreshCheck = 4;
+    int displayDataIndex = 0;
 
-    TextView hintText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drag_and_drop);
 
         topcontainer = (LinearLayout)findViewById(R.id.top_container);
-        hintText = (TextView)findViewById(R.id.hint_text);
-
+        answercontainer = (LinearLayout)findViewById(R.id.bottom_container);
+        checkMarkContainer = (LinearLayout)findViewById(R.id.check_mark_container);
 
         checkMark = findViewById(R.id.green_tick);
+
+
+        findViewById(R.id.top_container).setOnDragListener(this);
+        findViewById(R.id.bottom_container).setOnDragListener(this);
+
+
+        index = new Integer[]{0,1,2,3,4};
+        List<Integer> index_shuffle = Arrays.asList(index);
+        Collections.shuffle(index_shuffle);
+        Log.d("outofbounds", "arrived"+ index_shuffle.toString());
+        insertData();
+
+    }
+
+
+    public void insertData()
+    {
+
+        List<String[]> questions = new ArrayList<>();
 
         String[] question1 = new String[]{"안", "녕", "하", "세", "요"};
         String[] question2 = new String[]{"잘", "먹", "겠", "습", "니", "다"};
@@ -60,87 +87,92 @@ public class DemoDragAndDropActivity extends Activity implements View.OnDragList
         String[] question4 = new String[]{"여", "보", "세", "요"};
         String[] question5 = new String[]{"죄", "송", "합", "니", "다"};
 
+        questions.add(question1);
+        questions.add(question2);
+        questions.add(question3);
+        questions.add(question4);
+        questions.add(question5);
+
 
         String photo1 = "https://firebasestorage.googleapis.com/v0/b/team-default.appspot.com/o/demo%2Fgreet.jpg?alt=media&token=7b416195-2e37-41e4-afc6-dc47a079d397";
         String photo2 = "https://firebasestorage.googleapis.com/v0/b/team-default.appspot.com/o/demo%2Feat.jpg?alt=media&token=2d38803d-2004-412b-b8bd-c49bad036f2a";
         String photo3 = "https://firebasestorage.googleapis.com/v0/b/team-default.appspot.com/o/demo%2Fthanks.jpg?alt=media&token=bd38bf74-7860-45f9-a373-2dc79c2cec4f";
         String photo4 = "https://firebasestorage.googleapis.com/v0/b/team-default.appspot.com/o/demo%2Fhello.jpg?alt=media&token=ab5f9e4b-5f03-490d-bb05-c444cab434b0";
         String photo5 = "https://firebasestorage.googleapis.com/v0/b/team-default.appspot.com/o/demo%2Fsorry.jpg?alt=media&token=bc481f99-ee6e-46ac-9ce9-eff50b9fb5cf";
-        HashMap<String[], String> data = new HashMap<>();
 
-        data.put(question1, photo1);
-        data.put(question2, photo2);
-        data.put(question3, photo3);
-        data.put(question4, photo4);
-        data.put(question5, photo5);
+        List<String> photos = new ArrayList<>();
 
+        photos.add(photo1);
+        photos.add(photo2);
+        photos.add(photo3);
+        photos.add(photo4);
+        photos.add(photo5);
 
-        shuffle(data);
-
-
-        findViewById(R.id.top_container).setOnDragListener(this);
-        findViewById(R.id.bottom_container).setOnDragListener(this);
-
-
+        displayData(questions, photos);
 
     }
 
-    public void shuffle(HashMap<String[], String>toShuffle)
+    public void displayData(List<String[]> questions, List<String> photos)
     {
+        hashmapIndex = index[displayDataIndex];
 
-        List<String[]> answer = new ArrayList<>();
-        List<String> pictures = new ArrayList<>();
+        answerText = questions.get(hashmapIndex);
+        List<Integer> image_id_shuffle = new ArrayList<>();
+        List<String> image_shuffle = new ArrayList<>();
+        Random random = new Random();
+        image_shuffle.add(photos.get(hashmapIndex));
+        image_id_shuffle.add(hashmapIndex);
 
-        for(Object key: toShuffle.keySet())
+        HashMap<Integer, String> mapping = new HashMap<>();
+        mapping.put(hashmapIndex,photos.get(hashmapIndex));
+
+        while(image_shuffle.size()<3)
         {
-            String[] temp = (String[])key;
-            answer.add(temp);
-            pictures.add(toShuffle.get(key));
+            int randomImg = random.nextInt(5);
+            if(!image_shuffle.contains(photos.get(randomImg)))
+            {
+                image_id_shuffle.add(randomImg);
+                image_shuffle.add(photos.get(randomImg));
+                mapping.put(randomImg, photos.get(randomImg));
+            }
         }
 
 
-        Random random = new Random();
-        superIndex = random.nextInt(3);
-        Log.d("random", "index is:" + superIndex);
-
-        for(int i = 0; i<3; i++)
+        for (int i = 0; i<3; i++)
         {
-            ImageView imgTest = new ImageView(getApplicationContext());
-            imgTest.setId(i);
+            ImageView imgDisplay = new ImageView(getApplicationContext());
+            imgDisplay.setId(mapping.keySet(i));
 
             Picasso.get()
-                    .load(pictures.get(i))
+                    .load(image_shuffle.get(i))
                     .fit()
                     .centerInside()
-                    .into(imgTest);
+                    .into(imgDisplay);
 
-            Log.d("pictures", "pictures: "+ pictures.get(i));
-            Log.d("bug", ""+ pictures.toString());
-
-            topcontainer.addView(imgTest);
-            imgTest.getLayoutParams().height = 300;
-            imgTest.getLayoutParams().width = 300;
-            imgTest.setOnLongClickListener(DemoDragAndDropActivity.this::onLongClick);
+            topcontainer.addView(imgDisplay);
+            imgDisplay.getLayoutParams().height = 300;
+            imgDisplay.getLayoutParams().width= 300;
+            imgDisplay.setOnLongClickListener(DemoDragAndDropActivity.this::onLongClick);
         }
-
-        answerText = answer.get(superIndex);
-        String answerTextTemp = TextUtils.join("", answerText);
-        hintText.setText(answerTextTemp);
-
+        TextView hintText = new TextView(getApplicationContext());
+        String answerTextJoin = TextUtils.join("",answerText);
+        hintText.setText(answerTextJoin);
+        hintText.setId(0);
+        hintText.setTextSize(40);
+        answercontainer.addView(hintText);
+        displayDataIndex++;
     }
+
 
     public int checkAnswer(View draggedImageView)
     {
-        Log.d("drag", "draggedImageViewid" + draggedImageView.getId());
-        Log.d("drag", "superindex" + superIndex);
-        if(draggedImageView.getId() == superIndex)
+
+        if(draggedImageView.getId() == hashmapIndex)
         {
             return 1;
         }
         else return 0;
     }
-
-
 
 
     @Override
@@ -159,8 +191,7 @@ public class DemoDragAndDropActivity extends Activity implements View.OnDragList
         View draggedImageView = (View) dragEvent.getLocalState();
         ViewGroup draggedImageViewParentLayout = (ViewGroup)draggedImageView.getParent();
         LinearLayout bottomLinearLayout = (LinearLayout)receivingLayoutView;
-
-
+        Handler handler = new Handler();
 
         switch(dragEvent.getAction()){
 
@@ -208,27 +239,30 @@ public class DemoDragAndDropActivity extends Activity implements View.OnDragList
                         draggedImageViewParentLayout.removeView(draggedImageView);
                         bottomLinearLayout.addView(draggedImageView);
                         draggedImageView.setVisibility(View.VISIBLE);
-
+                        checkMark.getDuration();
                         if(bottomLinearLayout.getChildCount() == 2)
                         {
+                            checkMark.setVisibility(View.VISIBLE);
                             checkMark.playAnimation();
-                            checkMark.addAnimatorListener(new AnimatorListenerAdapter() {
+                            handler.postDelayed(new Runnable() {
                                 @Override
-                                public void onAnimationEnd(Animator animation) {
-                                   if(refreshCheck>0) {
-                                       topcontainer.removeAllViews();
-                                       bottomLinearLayout.removeAllViews();
-                                       onCreate(new Bundle());
-                                       refreshCheck--;
-                                   }
-                                   else{
-                                       Intent intent = new Intent(DemoDragAndDropActivity.this, EndScreenActivity.class);
-                                       intent.putExtra("unityScore", score);
-                                       startActivity(intent);
-                                       finish();
-                                   }
+                                public void run() {
+                                    if(refreshCheck>0) {
+                                        topcontainer.removeAllViews();
+                                        bottomLinearLayout.removeAllViews();
+                                        Log.d("checkmark", "called");
+                                        insertData();
+                                        checkMark.setVisibility(View.INVISIBLE);
+                                        refreshCheck--;
+                                    }
+                                    else{
+                                        Intent intent = new Intent(DemoDragAndDropActivity.this, EndScreenActivity.class);
+                                        intent.putExtra("unityScore", score);
+                                        startActivity(intent);
+                                        finish();
+                                    }
                                 }
-                        });
+                            }, checkMark.getDuration());
                         }
                         return true;
                     default:
